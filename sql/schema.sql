be4ddab557c4df06db3a416e3b1238f83632179a
Julia Ogris <julia.ogris@gmail.com>
2020-11-10 20:22:29 +1100

    sql: Update schema
    Remove `IF NOT EXITS` clause from table creation as we now intend to
    cover this case and schema upgrades in code.
    
    Add `CHECK` clause to ensure most TEXT fields don't contain empty
    strings, which is what we want and accidentally forgot about earlier.

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,24 +1,23 @@
-CREATE TABLE IF NOT EXISTS users (
-	name          TEXT PRIMARY KEY,
-	password_hash TEXT NOT NULL,
+CREATE TABLE users (
+	name          TEXT PRIMARY KEY CHECK(name <> ''),
+	password_hash TEXT NOT NULL CHECK(password_hash <> ''),
 	avatar        BLOB
 );
 
-CREATE TABLE IF NOT EXISTS rooms (
-	name TEXT PRIMARY KEY
+CREATE TABLE rooms (
+	name TEXT PRIMARY KEY CHECK(name <> '')
 );
 
-CREATE TABLE IF NOT EXISTS messages (
+CREATE TABLE messages (
 	id         INTEGER PRIMARY KEY,
-	content    TEXT NOT NULL,
-	created_at TEXT NOT NULL, -- rfc3339: 2019-10-25T07:55:50Z
+	content    TEXT NOT NULL CHECK(content <> ''),
+	created_at TEXT NOT NULL CHECK(created_at <> ''), -- rfc3339: 2019-10-25T07:55:50Z
 	room       TEXT NOT NULL REFERENCES rooms(name),
 	author     TEXT NOT NULL REFERENCES users(name)
 );
 
-CREATE TABLE IF NOT EXISTS schema (
-	version TEXT PRIMARY KEY
+CREATE TABLE schema (
+	version TEXT PRIMARY KEY CHECK(version <> '')
 );
 
--- Only insert version into empty schema table.
-INSERT INTO schema SELECT 'v0.0.1' WHERE NOT EXISTS (SELECT * FROM schema);
+INSERT INTO schema VALUES ('v0.0.1');
