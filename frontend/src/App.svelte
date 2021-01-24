624f9bb041eee7e38656b259ee30f8a07b024316
Julia Ogris <julia.ogris@gmail.com>
2021-01-25 10:38:16 +1100

    ui: Add tailwind setup
    Tailwind provides a design framework and functional CSS which seem cool
    and popular so we will try working with them here.
    See https://tailwindcss.com/ for more.
    
    Instructions for this setup where taken from various blogs and
    https://github.com/chrisdhanaraj/svelte-tailwind-integration

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
+  <h1 class="text-red-400 text-2xl py-8">Hello {name}!</h1>
   <p>
     Visit the
     <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
@@ -11,24 +11,14 @@
   </p>
 </main>
 
-<style>
+<style global>
+  /* purgecss start ignore */
+  @tailwind base;
+  @tailwind components;
+  /* purgecss end ignore */
+  @tailwind utilities;
+
   main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+    @apply max-w-sm mx-auto;
   }
 </style>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,7 @@
+const production = !process.env.ROLLUP_WATCH
+module.exports = {
+  purge: {
+    content: ["./src/**/*.svelte"],
+    enabled: production,
+  },
+}
